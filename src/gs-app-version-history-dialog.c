offsoc/gnome-software

gs-app-version-history-dialog: Add row for current version if not provided
The appstream data may not always be up-to-date with the app version, like
with package-based apps, thus add a row for the installed version, in case
it's missing in the list.

src/gs-app-version-history-dialog.c
@@ -30,7 +30,9 @@ populate_version_history (GsAppVersionHistoryDialog *dialog,
 {
 	g_autoptr(GPtrArray) version_history = NULL;
 	const gchar *app_version;
+	GtkWidget *installed_row = FALSE;
 	gboolean can_installed;
+	gboolean have_installed;
 
 	/* remove previous */
 	gs_widget_remove_all (dialog->listbox, (GsRemoveFunc) gtk_list_box_remove);
@@ -53,6 +55,7 @@ populate_version_history (GsAppVersionHistoryDialog *dialog,
 
 	app_version = gs_app_get_version_ui (app);
 	can_installed = app_version != NULL && version_history->len > 1 && gs_app_is_installed (app);
+	have_installed = !can_installed;
 
 	/* add each */
 	for (guint i = 0; i < version_history->len; i++) {
@@ -60,8 +63,27 @@ populate_version_history (GsAppVersionHistoryDialog *dialog,
 		AsRelease *version = g_ptr_array_index (version_history, i);
 		gboolean is_installed = FALSE;
 
-		if (can_installed)
-			is_installed = g_strcmp0 (as_release_get_version (version), app_version) == 0;
+		if (app_version != NULL) {
+			gint vercmp = as_vercmp (as_release_get_version (version), app_version, AS_VERCMP_FLAG_NONE);
+			if (can_installed) {
+				is_installed = vercmp == 0;
+				have_installed = have_installed || is_installed;
+				/* in case the versions are in some odd order */
+				if (is_installed && installed_row != NULL)
+					gtk_widget_set_visible (installed_row, FALSE);
+			}
+			/* add the installed version just before the first lower version if not part of the list */
+			if (!have_installed && vercmp < 0) {
+				have_installed = TRUE;
+				row = gs_app_version_history_row_new ();
+				gs_app_version_history_row_set_always_expanded (GS_APP_VERSION_HISTORY_ROW (row), TRUE);
+				gs_app_version_history_row_set_info (GS_APP_VERSION_HISTORY_ROW (row),
+								     app_version,
+								     gs_app_get_release_date (app), NULL, TRUE);
+				gtk_list_box_append (GTK_LIST_BOX (dialog->listbox), row);
+				installed_row = row;
+			}
+		}
 
 		row = gs_app_version_history_row_new ();
 		gs_app_version_history_row_set_always_expanded (GS_APP_VERSION_HISTORY_ROW (row), TRUE);

itsManjeet/softwares

src/gs-app-version-history-dialog.c
@@ -30,7 +30,9 @@ populate_version_history (GsAppVersionHistoryDialog *dialog,
 {
 	g_autoptr(GPtrArray) version_history = NULL;
 	const gchar *app_version;
+	GtkWidget *installed_row = NULL;
 	gboolean can_check_whether_the_version_is_installed;
+	gboolean have_installed;
 
 	/* remove previous */
 	gs_widget_remove_all (dialog->listbox, (GsRemoveFunc) gtk_list_box_remove);
@@ -53,6 +55,7 @@ populate_version_history (GsAppVersionHistoryDialog *dialog,
 
 	app_version = gs_app_get_version_ui (app);
 	can_check_whether_the_version_is_installed = app_version != NULL && version_history->len > 1 && gs_app_is_installed (app);
+	have_installed = !can_check_whether_the_version_is_installed;
 
 	/* add each */
 	for (guint i = 0; i < version_history->len; i++) {
@@ -60,8 +63,27 @@ populate_version_history (GsAppVersionHistoryDialog *dialog,
 		AsRelease *version = g_ptr_array_index (version_history, i);
 		gboolean is_installed = FALSE;
 
-		if (can_check_whether_the_version_is_installed)
-			is_installed = g_strcmp0 (as_release_get_version (version), app_version) == 0;
+		if (app_version != NULL) {
+			gint vercmp = as_vercmp (as_release_get_version (version), app_version, AS_VERCMP_FLAG_NONE);
+			if (can_check_whether_the_version_is_installed) {
+				is_installed = vercmp == 0;
+				have_installed = have_installed || is_installed;
+				/* in case the versions are in some odd order */
+				if (is_installed && installed_row != NULL)
+					gtk_widget_set_visible (installed_row, FALSE);
+			}
+			/* add the installed version just before the first lower version if not part of the list */
+			if (!have_installed && vercmp < 0) {
+				have_installed = TRUE;
+				row = gs_app_version_history_row_new ();
+				gs_app_version_history_row_set_always_expanded (GS_APP_VERSION_HISTORY_ROW (row), TRUE);
+				gs_app_version_history_row_set_info (GS_APP_VERSION_HISTORY_ROW (row),
+								     app_version,
+								     gs_app_get_release_date (app), NULL, TRUE);
+				gtk_list_box_append (GTK_LIST_BOX (dialog->listbox), row);
+				installed_row = row;
+			}
+		}
 
 		row = gs_app_version_history_row_new ();
 		gs_app_version_history_row_set_always_expanded (GS_APP_VERSION_HISTORY_ROW (row), TRUE);